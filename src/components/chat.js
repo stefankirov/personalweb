e87605ee4fe1f625c6ad7a2c13e445b0b5b0d032
Stefan Kirov <stefankirov@gmail.com>
2026-04-18 23:24:51 -0700

    icon

--- src/components/chat.js
+++ src/components/chat.js
@@ -7,7 +7,9 @@ export function renderChat() {
   <button id="chat-toggle" aria-label="Chat with Stefan's Virtual Self" aria-expanded="false">
     <span class="chat-toggle-open">
       <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
-        <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+        <path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/>
+        <path d="M20 3v4"/><path d="M22 5h-4"/>
+        <path d="M4 17v2"/><path d="M5 18H3"/>
       </svg>
       <span>Ask Stefan</span>
     </span>
